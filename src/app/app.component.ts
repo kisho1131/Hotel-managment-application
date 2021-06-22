631bae3d079792925d2050bb182d4639f7cc82c9
kisho1131 <sumitkishore0131@gmail.com>
2021-06-22 21:17:03 +0530

    Updating Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-header></app-header>
     <app-home></app-home>
-     <app-login></app-login>
+    <app-login></app-login>
   `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
